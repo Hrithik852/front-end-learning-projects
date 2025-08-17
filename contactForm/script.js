5bf2f1f79d1b6391cc4e089f72a6502e7f0e357b
HRITHIK suresh <hrithiksuresh852@gmail.com>
2025-08-17 21:20:36 +0530

    contact form as per aim completed

--- contactForm/script.js
+++ contactForm/script.js
@@ -1,3 +1,35 @@
 let subBtn=document.querySelector("#submit");
 let form=document.querySelector("form")
-form.addEventListener("submit",(e)=>{e.preventDefault();})
+let nameerror=document.querySelector(".nameerror");
+let Name=document.querySelector("#name");
+let email=document.querySelector("#email");
+let valid;
+
+ form.addEventListener("submit",(e)=>{e.preventDefault();
+      if(Name.value.trim().length<3){
+       nameerror.textContent="name should be more than 2 characters";
+       document.getElementById("nameerror").style.display="block";
+       valid=false;
+    }
+else{nameerror.textContent="";
+  console.log(Name.value);
+       document.getElementById("nameerror").style.display="none";
+       valid=true
+      }
+     let emailregex=/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    console.log("hs"+email.value);
+     let isValid=emailregex.test(email.value);
+      if(isValid){
+        document.getElementById("emailerror").style.display="none";
+      }
+      else{
+        emailerror.textContent="email is invalid";
+        document.getElementById("emailerror").style.display="block";
+       valid=false;
+      }
+      if(valid){
+document.querySelectorAll(".input").forEach(e=>e.value="");
+document.querySelector("textarea").value="";
+    alert("form succesfully submitted")
+      }
+ })
