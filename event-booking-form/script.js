f39c82857a2b6eeedb614492b036fe6c1827a0e4
HRITHIK suresh <hrithiksuresh852@gmail.com>
2025-08-19 23:57:58 +0530

    style and structure created

--- event-booking-form/script.js
+++ event-booking-form/script.js
@@ -1,1 +1,9 @@
 console.log("hey")
+let form=document.querySelector("form");
+let name=document.getElementById("name");
+let number=document.getElementById("number");
+let tickets=document.getElementById("tickets");
+const name_regex=/^[a-zA-Z]+(?:[' -][a-zA-Z]+)*$/;
+const ph_regex=/^(?:(?:\+91)|0)?[ -]?[6789]\d{9}$/;
+const no_of_tickets_regex=/^[1-5]$/;
+form.addEventListener("submit",(e)=>{e.preventDefault();})
