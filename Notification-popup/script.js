44e9627f6a050ac0b0958a3d3387475cb68d4ecf
HRITHIK suresh <hrithiksuresh852@gmail.com>
2025-08-20 23:38:34 +0530

    popup-Notification successfull

--- Notification-popup/script.js
+++ Notification-popup/script.js
@@ -1,1 +1,8 @@
-console.log("hey")
+let box=document.getElementById("popup");
+let x=document.querySelector(".close")
+setTimeout(()=>{box.style.display="inline";
+console.log("hh")},2000);
+
+x.addEventListener("click",()=>{
+    box.style.display="none";
+ })
