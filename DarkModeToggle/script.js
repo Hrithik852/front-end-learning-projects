cc0bfc5f3323ce2144997f4a7e58516a482191d6
HRITHIK suresh <hrithiksuresh852@gmail.com>
2025-09-02 23:10:26 +0530

    DarkModeToggle using localStorage
    pointer added for caraousal

---
+++ DarkModeToggle/script.js
@@ -0,0 +1,22 @@
+let btn=document.querySelector(".toggle")
+if(localStorage.getItem("theme")=="dark")
+{
+    document.body.classList.add("dark");
+    console.log(localStorage.getItem("theme"))
+}
+else{
+    console.log(localStorage.getItem("theme"))
+    document.body.classList.add("white")
+}
+btn.addEventListener("click",()=>{
+
+    document.body.classList.toggle("dark");
+    if(document.body.classList.contains("dark"))
+    {
+        localStorage.setItem("theme","dark");        
+    }
+    else{
+        localStorage.setItem("theme","white");        
+
+    }
+})
