8059fab4497bc59598666b6715faa0fdb3c9903d
HRITHIK suresh <hrithiksuresh852@gmail.com>
2025-09-03 00:22:57 +0530

    username saver done

--- RememberMe/script.js
+++ RememberMe/script.js
@@ -1,2 +1,27 @@
-let btn=document.querySelector("button").addEventListener("click",(e)=>{e.preventDefault()})
+let btn=document.querySelector("button");
 let ck=document.getElementById("cb");
+let input=document.getElementById("un");
+ if(localStorage.getItem("name"))
+    {
+        input.value=localStorage.getItem("name");
+    }
+btn.addEventListener("click",(e)=>{e.preventDefault();
+   
+
+     
+  
+
+if(ck.checked&&input.value.trim()!="")
+{
+    localStorage.setItem("name",input.value);
+}
+else if(ck.checked)
+{
+    localStorage.removeItem("name");
+}
+     if(input.value.trim()!="")
+    {
+        input.value="";
+  }
+    }
+)
